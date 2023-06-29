bae2e216eadc9f35a049f02e7047e67bf4211f2c
USEN GIT CONFIG --LOCAL <USEN GIT CONFIG --LOCAL>
2023-06-29 13:36:37 -0600

    Holaaaa

---
+++ Servlets3a/src/main/java/mx/edu/utez/servlets3a/model/person/BeanPerson.java
@@ -0,0 +1,6 @@
+package mx.edu.utez.servlets3a.model.person;
+
+public class BeanPerson {
+    private long id;
+
+}
---
+++ Servlets3a/src/main/java/mx/edu/utez/servlets3a/model/person/DaoPerson.java
@@ -0,0 +1,4 @@
+package mx.edu.utez.servlets3a.model.person;
+
+public class DaoPerson {
+}
